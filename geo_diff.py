9d30afa33bee79a573f22646f66c4e643ce00d1d
Edoardo <tesei.edoardo997@gmail.com>
2025-02-11 09:54:23 +0100

    Update geo_diff.py

--- geo_diff.py
+++ geo_diff.py
@@ -1,7 +1,5 @@
 import sympy as sp
 from sympy import Matrix, diff, MutableDenseNDimArray
-
-import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
